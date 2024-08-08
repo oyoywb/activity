206e98f46eb51b2ec2f34795d370738f15337895
oywb <1196829219@qq.com>
2024-08-08 21:38:11 +0800

    fix-240303 test

--- src/main/java/com/oywb/weixin/activities/entity/ActivityEntityNew.java
+++ src/main/java/com/oywb/weixin/activities/entity/ActivityEntityNew.java
@@ -2,11 +2,14 @@ package com.oywb.weixin.activities.entity;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "activity", schema = "oywb_test", catalog = "")
 public class ActivityEntityNew extends ActivityEntity {
+
+    @Column(name = "is_added_to_plan")
     private boolean isAddedToPlan;
 }
